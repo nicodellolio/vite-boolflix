e9bb0a909ced4a2a6e346489ab97a3d3840ff5d5
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-04-04 15:58:24 +0200

    add stars rating count - MILESTONE 3

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,7 +10,8 @@ export default {
     return {
       state,
       countries,
-      poster_base: 'https://image.tmdb.org/t/p/w342'
+      poster_base: 'https://image.tmdb.org/t/p/w342',
+      ratingString: ''
     }
   },
   methods: {
@@ -21,10 +22,9 @@ export default {
       this.getMovie(`https://api.themoviedb.org/3/search/multi?api_key=dac5f4cfb17fc8dc2333867ac5b06c0b&query=${this.state.searchingText}`)
     },
 
-  },
-  mounted() {
-  }
 
+  },
+  mounted() { }
 }
 </script>
 
@@ -42,41 +42,78 @@ export default {
 
     <ul>
       <li v-for="movie in state.movies">
-        <div class="box">
 
 
-          <div class="poster-box">
-            <img v-if="movie.poster_path != null" :src="poster_base + movie.poster_path" alt="">
-            <img style="width: 342px;" v-else src="/noimage.png" alt="">
-          </div>
-
-          <h3 v-if="movie.media_type == 'movie'">
-            {{ movie.title }}
-          </h3>
-          <h3 v-else-if="movie.media_type == 'tv'">
-            {{ movie.name }}
-          </h3>
-
-          <aside v-if="movie.media_type == 'movie'">({{ movie.original_title }})
-          </aside>
-          <aside v-else-if="movie.media_type == 'tv'">({{ movie.original_name }})
-          </aside>
-
-
-          <div class="language-box">
-
-            <div v-for="flag in countries.flags"></div>
-            <img v-if="movie.original_language" style="width: 30px;" :src="`flags/4x3/${movie.original_language}.svg`"
-              alt="">
-            <h4 v-else>{{ movie.original_language }}</h4>
-            <h4>{{ movie.media_type }}</h4>
-
-          </div>
-
-          <h4>{{ Math.ceil(movie.vote_average / 2) }}</h4>
+        <div class="poster-box">
+          <img v-if="movie.poster_path != null" :src="poster_base + movie.poster_path" alt="">
+          <img style="width: 342px;" v-else src="/noimage.png" alt="">
         </div>
-      </li>
 
+        <h3 v-if="movie.media_type == 'movie'">
+          {{ movie.title }}
+        </h3>
+        <h3 v-else-if="movie.media_type == 'tv'">
+          {{ movie.name }}
+        </h3>
+
+        <aside v-if="movie.media_type == 'movie'">({{ movie.original_title }})
+        </aside>
+        <aside v-else-if="movie.media_type == 'tv'">({{ movie.original_name }})
+        </aside>
+
+
+        <section class="language-box">
+
+          <div v-for="flag in countries.flags"></div>
+          <img v-if="movie.original_language" style="width: 30px;" :src="`flags/4x3/${movie.original_language}.svg`"
+            alt="">
+          <h4 v-else>{{ movie.original_language }}</h4>
+          <h4>{{ movie.media_type }}</h4>
+
+        </section>
+
+        <section class="rating">
+
+          <div v-if="Math.ceil(movie.vote_average / 2) <= 1" class="ratin-bad">
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+          </div>
+          <div v-else-if="Math.ceil(movie.vote_average / 2) == 2" class="rating-not-bad">
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+          </div>
+          <div v-else-if="Math.ceil(movie.vote_average / 2) == 3" class="rating-ok">
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+          </div>
+          <div v-else-if="Math.ceil(movie.vote_average / 2) == 4" class="ratin-good">
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+          </div>
+          <div v-else-if="Math.ceil(movie.vote_average / 2) == 5" class="rating-very-good">
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-solid fa-star" aria-hidden="true"></i>
+            <i class="fa-regular fa-star" aria-hidden="true"></i>
+          </div>
+
+        </section>
+
+      </li>
+      
     </ul>
   </div>
 </template>
